32cd12bf121ff447876ba8f525b1ca6e122fd1f0
SaifulIslamFoysal <saifulytube47@gmail.com>
2024-10-23 13:38:03 +0600

    Updates

---
+++ precache-manifest.20a6d5d3a12e72af74314759665edf3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6591ab0858c2617d27d29968d08d810",
+    "url": "/NewsMonkey/index.html"
+  },
+  {
+    "revision": "0884e39ec810a85b0dc7",
+    "url": "/NewsMonkey/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "4b200f5ec890351d8d10",
+    "url": "/NewsMonkey/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0884e39ec810a85b0dc7",
+    "url": "/NewsMonkey/static/js/2.a711838c.chunk.js"
+  },
+  {
+    "revision": "75d7bab2af96fc36c013",
+    "url": "/NewsMonkey/static/js/3.e77cf3e3.chunk.js"
+  },
+  {
+    "revision": "4b200f5ec890351d8d10",
+    "url": "/NewsMonkey/static/js/main.1cf412d9.chunk.js"
+  },
+  {
+    "revision": "ff1ba669a8735de89aa9",
+    "url": "/NewsMonkey/static/js/runtime~main.5b170a80.js"
+  },
+  {
+    "revision": "ce797770335edc42003a9c875280b426",
+    "url": "/NewsMonkey/static/media/loader.ce797770.gif"
+  }
+]);
